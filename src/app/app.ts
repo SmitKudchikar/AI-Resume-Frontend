10b843b7e61ebce3c71dff4bfcd8bf9ad553c8b7
SMIT KUDCHIKAR <smitkudchikar67@gmail.com>
2026-07-01 23:51:23 +0530

    resume-form component removed

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ResumeForm } from "./components/resume-form/resume-form";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ResumeForm],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
