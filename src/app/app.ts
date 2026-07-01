2c40c0cdbf1749b8f224856ad11f9d880e553507
SMIT KUDCHIKAR <smitkudchikar67@gmail.com>
2026-07-01 20:52:35 +0530

    changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { ResumeForm } from './components/resume-form/resume-form';
+import { RouterOutlet } from "@angular/router";
 
 
 @Component({
   selector: 'app-root',
     standalone: true,
-  imports: [ResumeForm],
+  imports: [ResumeForm, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
